0ec823da7060c318eba568494b687d128bbaf22e
ninadelvoix <130978622+ninadelvoix@users.noreply.github.com>
2023-06-20 17:37:39 +0200

    Add files via upload

---
+++ web-typography-22-23/closed-captions/sounds.js
@@ -0,0 +1,22 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+	0, // dit zijn de sounds, kan je zelf toevoegen als je op een moment iets wil maar hier is geen sound.
+	3,
+	9,
+	12,
+	15,
+	16.5,
+	24,
+	25.8,
+	31,
+	32,
+	33,
+	34,
+	75,
+	86,
+	89,
+	102,
+	136,
+	
+];
