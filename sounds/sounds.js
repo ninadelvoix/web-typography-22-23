be267e94cb24c58d80a216d0f0f3c96d4f95b6f7
ninadelvoix <130978622+ninadelvoix@users.noreply.github.com>
2023-06-20 15:43:44 +0200

    Add files via upload

---
+++ sounds/sounds.js
@@ -0,0 +1,15 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+	0, // dit zijn de sounds, kan je zelf toevoegen als je op een moment iets wil maar hier is geen sound.
+	9,
+	12,
+	15,
+	16.5,
+	24,
+	25.8,
+	31,
+	32,
+	34,
+	86
+];
